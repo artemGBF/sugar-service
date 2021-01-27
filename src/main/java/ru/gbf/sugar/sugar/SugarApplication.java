3db7cff9954dbf67b9a8f1b983c0c472ee784cf8
eugeny1964 <sizovzhenya1964@yandex.ru>
2021-01-27 16:55:48 +0300

    Добавлено сообщение-приветствие в CommandLinerinner (#1)
    * Добавлено сообщение-приветствие в CommandLinerinner
    
    * Добавлено сообщение-приветствие в CommandLinerinner

--- src/main/java/ru/gbf/sugar/sugar/SugarApplication.java
+++ src/main/java/ru/gbf/sugar/sugar/SugarApplication.java
@@ -1,13 +1,18 @@
 package ru.gbf.sugar.sugar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SugarApplication {
+public class SugarApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SugarApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello!");
+    }
 }
