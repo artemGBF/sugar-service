ab11e1fcd97c8613bf9a93178c3e385504a33c87
eugeny1964 <sizovzhenya1964@yandex.ru>
2021-02-12 18:11:10 +0300

    Не (#6)
    Added integration with yandex.disk

--- src/main/java/ru/gbf/sugar/sugar/controller/Controller.java
+++ src/main/java/ru/gbf/sugar/sugar/controller/Controller.java
@@ -1,20 +1,45 @@
 package ru.gbf.sugar.sugar.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.gbf.sugar.sugar.entity.Sugar;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.gbf.sugar.sugar.servise.SugarServise;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "sugar")
 public class Controller {
 
+    @Autowired
+    private SugarServise sugarServise;
+
     @GetMapping("/getall")
-    public List<Sugar> getAll() {
-        return null;
+    public String getAll() throws IOException {
+        String all = sugarServise.getAll();
+        return all;
     }
+
+    @GetMapping("/getauth")
+    public String getAuth(@RequestParam String code) throws IOException   {
+        String auth = sugarServise.getAuth(code);
+        return auth;
+    }
+    
 
     @GetMapping("/getbyid/{id}")
     public Sugar getbyid(Long id) {
